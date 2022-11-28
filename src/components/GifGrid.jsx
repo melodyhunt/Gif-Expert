b14cea25e4af6c0849e7a832edbf2bf2b547bb0e
Melody Hunt <mariamelodyhunt@hotmail.com>
2022-11-28 12:11:15 -0300

    Responsive

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -11,7 +11,7 @@ export const GifGrid = ({ category }) => {
       </h3>
       {isLoading && <h4>Cargando...</h4>}
 
-      <div className=" grid grid-cols-2 md:grid-cols-4  mx-10 md:mx-20 ">
+      <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4  mx-10 md:mx-20 ">
         {images.map((image) => (
           <GifGridItems key={image.id} {...image} />
         ))}
